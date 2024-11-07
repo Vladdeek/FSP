43f003aecd47de7abc3790e9e35559e9b2c01a1b
Vladdeek <126556608+Vladdeek@users.noreply.github.com>
2024-11-07 13:19:09 +0200

    ADD mobile navigation hamburger menu

--- ui/header.php
+++ ui/header.php
@@ -6,15 +6,28 @@
         </div>
         <p class="full-name">федерация спортивного программирования МЛТ</p>
     </div>
-    
-   <div class="navigation d-flex flex-wrap">
-        <a href="#" class="nav-link text-uppercase">о федерации</a>
-        <a href="#" class="nav-link text-uppercase">структура</a>
-        <a href="#" class="nav-link text-uppercase">дисциплины</a>
-        <a href="#" class="nav-link text-uppercase">мероприятия</a>
-        <a href="#" class="nav-link text-uppercase">новости</a>
-        <a href="#" class="nav-link text-uppercase">галерея</a>
-        <a href="#" class="nav-link text-uppercase">контакты</a>
-    </div>
-    <img class="theme-btn" onclick="changeTheme()"src="img/free-icon-half-moon-8098397.png" alt="Иконка">
+
+    <nav class="navbar navbar-expand-lg ms-auto">
+        <div class="container-fluid">
+            <!-- Кнопка бургер меню для небольших экранов -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Выпадающее меню -->
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <div class="navigation d-flex flex-wrap">
+                    <a href="#" class="nav-link text-uppercase">о федерации</a>
+                    <a href="#" class="nav-link text-uppercase">структура</a>
+                    <a href="#" class="nav-link text-uppercase">дисциплины</a>
+                    <a href="#" class="nav-link text-uppercase">мероприятия</a>
+                    <a href="#" class="nav-link text-uppercase">новости</a>
+                    <a href="#" class="nav-link text-uppercase">галерея</a>
+                    <a href="#" class="nav-link text-uppercase">контакты</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <img class="theme-btn" onclick="changeTheme()" src="img/free-icon-half-moon-8098397.png" alt="Иконка">
 </div>
