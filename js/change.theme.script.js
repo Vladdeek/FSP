cd55a32692960a234602fc176404c99cf3f613d9
Vladdeek <126556608+Vladdeek@users.noreply.github.com>
2024-11-07 17:17:40 +0200

    ADD new gradient for dark theme

--- js/change.theme.script.js
+++ js/change.theme.script.js
@@ -2,7 +2,6 @@ function changeTheme() {
 	const button = document.querySelector('.theme-btn')
 	const iconImage = button
 	const root = document.documentElement
-	const gradientText = document.querySelector('.name-slide')
 
 	// Меняем изображение и цвета
 	if (iconImage.src.includes('img/free-icon-half-moon-8098397.png')) {
@@ -10,10 +9,18 @@ function changeTheme() {
 		root.style.setProperty('--text-color', '#f1f1f1') // Инвертированный текст
 		root.style.setProperty('--bg-color', '#121212') // Инвертированный фон
 		root.style.setProperty('--color', '#232323') // Инвертированный цвет
+		root.style.setProperty(
+			'--gradient',
+			'linear-gradient(45deg, rgba(72, 0, 255, 1) 0%, rgba(252, 69, 202, 1) 100%)'
+		)
 	} else {
 		iconImage.src = 'img/free-icon-half-moon-8098397.png' // Путь к изображению для дневной темы
 		root.style.setProperty('--text-color', '#1b1c21') // Текст для дневной темы
 		root.style.setProperty('--color', '#ededed') // Фон для дневной темы
 		root.style.setProperty('--bg-color', '#eaeaea') // Цвет для дневной темы
+		root.style.setProperty(
+			'--gradient',
+			'linear-gradient(45deg,rgba(255, 106, 0, 1) 0%,rgba(238, 9, 121, 1) 50%,rgba(0, 154, 255, 1) 100%)'
+		)
 	}
 }
