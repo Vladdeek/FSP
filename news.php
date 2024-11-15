2acf8d0ed9c54a29f2d5f240f39e12aefee98351
Vladdeek <126556608+Vladdeek@users.noreply.github.com>
2024-11-15 08:43:38 +0200

    ADD 3 new pages

---
+++ news.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Федерация спортивного программирования</title>
+    <link rel="shortcut icon" href="logo.png" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <?php include("ui/header.php");?>
+    </header>
+    <section>
+        <?php include("ui/slider.php");?>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-11">
+                    <p class="description text-center">
+                        <strong>"Федерация спортивного программирования ЛНР"</strong> - региональное отделение Общероссийской физкультурно-спортивной общественной организации «Федерация спортивного программирования России» по Луганской Народной Республике.
+                    </p> 
+                </div>
+                <div class="container">
+                    <div class="col-12">
+                        <p class="title text-start">Новости</p>
+                        <div class="row">
+                            <?php include("ui/info-card.php");?>
+                            <?php include("ui/info-card.php");?>
+                            <?php include("ui/info-card.php");?>
+                            <?php include("ui/info-card.php");?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <?php include("ui/footer.php");?>
+    </footer>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/change.theme.script.js"></script>
+    <script src="js/auto.slider.script.js"></script>
+</body>
+</html>
--- ui/header.php
+++ ui/header.php
@@ -18,9 +18,9 @@
                 <div class="navigation d-flex flex-wrap">
                     <a href="about.php" class="nav-link text-uppercase">о федерации</a>
                     <a href="#" class="nav-link text-uppercase">структура</a>
-                    <a href="#" class="nav-link text-uppercase">дисциплины</a>
-                    <a href="#" class="nav-link text-uppercase">мероприятия</a>
-                    <a href="#" class="nav-link text-uppercase">новости</a>
+                    <a href="descepline.php" class="nav-link text-uppercase">дисциплины</a>
+                    <a href="mer.php" class="nav-link text-uppercase">мероприятия</a>
+                    <a href="news.php" class="nav-link text-uppercase">новости</a>
                     <a href="#" class="nav-link text-uppercase">галерея</a>
                     <a href="#" class="nav-link text-uppercase">контакты</a>
                 </div>
